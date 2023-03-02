90511f8e880a58220d7acde1a6b5641b0e5c7ed2
ibukunAK <ainaibukun01@yahoo.com>
2023-03-02 19:28:06 +0000

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,9 +3,9 @@
 
 /**
  * rot13 - encoder rot13
- *   * @s: pointer to string params
- *    *
- *     * Return: *s
+ * @s: pointer to string params
+ *
+ * Return: *s
  */
 
 char *rot13(char *s)
@@ -18,10 +18,12 @@ char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
-}
+{
 if (s[i] == data1[j])
 {
 s[i] = datarot[j];
 break;
+}
+}
 }
 }
