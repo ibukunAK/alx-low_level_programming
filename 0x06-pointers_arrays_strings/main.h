14fbbc8d5adfee048d893c75d0257cddbc588efe
ibukunAK <ainaibukun01@yahoo.com>
2023-03-03 08:13:11 -0800

    correction

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,5 @@ char *leet(char *a);
 char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-
+void print_buffer(char *b, int size);
 #endif
