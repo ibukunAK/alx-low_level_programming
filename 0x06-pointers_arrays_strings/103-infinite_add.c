b8b33a51318f985ab8e3ac8e3603eca80597543c
ibukunAK <ainaibukun01@yahoo.com>
2023-03-03 12:55:26 -0800

    104-print

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,8 +3,8 @@
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
- *  * @n2: text representation of 2nd number to add
- *   * @r: pointer to buffer
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
  */
