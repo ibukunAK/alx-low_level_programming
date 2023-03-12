c3ddf8a68339782fc58b14ea6c0816f4f9c84df3
ibukunAK <ainaibukun01@yahoo.com>
2023-03-12 04:19:59 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * *str:   parameter
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
