8940e5263228110edc20b65c5f16578a35b6b337
ibukunAK <ainaibukun01@yahoo.com>
2023-03-16 08:29:25 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,9 +5,9 @@
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
+ *
  * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
@@ -19,7 +19,7 @@ len2++;
 if (n < len2)
 s = malloc(sizeof(char) * (len1 + n + 1));
 else
-= malloc(sizeof(char) * (len1 + len2 + 1));
+s = malloc(sizeof(char) * (len1 + len2 + 1));
 if (!s)
 return (NULL);
 while (i < len1)
